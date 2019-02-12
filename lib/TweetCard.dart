62cdbf3ea1f9bf0b5a518f95276d9dc15522289f
Michael Gosling <contact@michaelgosling.me>
2019-02-12 01:19:57 +0000

    Replace retweet button with like button. Long press card now retweets.

--- lib/TweetCard.dart
+++ lib/TweetCard.dart
@@ -11,7 +11,7 @@ class TweetCard extends Card {
   );
   final buttonShape = RoundedRectangleBorder(
     borderRadius: BorderRadius.circular(20.0),
-    side: BorderSide(color: Colors.black, width: 0.3, style: BorderStyle.solid),
+    side: BorderSide(color: Colors.black, width: 0.1, style: BorderStyle.solid),
   );
 
   final Tweet tweet;
@@ -48,67 +48,77 @@ class TweetCard extends Card {
     }
 
     return Card(
-        elevation: 0,
-        shape: tweetShape,
-        child: Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 5),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            elevation: 0,
+            shape: tweetShape,
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 5),
+                child: GestureDetector(
+                onLongPress: () {
+                  // TODO: retweet code
+                  Scaffold.of(context).showSnackBar(new SnackBar(content: Text("Retweet"),));
+                },
+                child: Column(
                   children: <Widget>[
-                      Icon(Icons.person, size: 60),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(
-                          tweet.name,
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.w700),
-                          textAlign: TextAlign.start,
+                        Icon(Icons.person, size: 60),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tweet.name,
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w700),
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              "@" + tweet.handle + " – " + dateTimeDisplay,
+                              style: TextStyle(
+                                  fontSize: 16, color: Colors.black54),
+                              textAlign: TextAlign.start,
+                            )
+                          ],
                         ),
-                        Text(
-                          "@" + tweet.handle + " – " + dateTimeDisplay,
-                          style: TextStyle(fontSize: 16, color: Colors.black54),
-                          textAlign: TextAlign.start,
-                        )
+                        IconButton(
+                          onPressed: () {
+                            return true;
+                          },
+                          icon: Icon(Icons.share),
+                        ),
                       ],
                     ),
-                    IconButton(
-                        onPressed: () {return true;},
-                        icon: Icon(Icons.share),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                      child: Text(tweet.message),
+                    ),
+                    // Retweet/Reply
+                    ButtonBar(
+                      alignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        OutlineButton(
+                          onPressed: () {
+                            return true;
+                          },
+                          textTheme: ButtonTextTheme.accent,
+                          padding: EdgeInsets.all(0),
+                          shape: buttonShape,
+                          child: Text("like"),
+                        ),
+                        OutlineButton(
+                          onPressed: () {
+                            return true;
+                          },
+                          textTheme: ButtonTextTheme.accent,
+                          padding: EdgeInsets.all(0),
+                          shape: buttonShape,
+                          child: Text("reply"),
+                        ),
+                      ],
                     ),
                   ],
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
-                  child: Text(tweet.message),
-                ),
-                // Retweet/Reply
-                ButtonBar(
-                  alignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    OutlineButton(
-                      onPressed: () { return true;},
-                      textTheme: ButtonTextTheme.accent,
-                      padding: EdgeInsets.all(0),
-                      shape: buttonShape,
-                      child: Text("retweet"),
-                    ),
-                    OutlineButton(
-                      onPressed: () { return true;},
-                      textTheme: ButtonTextTheme.accent,
-                      padding: EdgeInsets.all(0),
-                      shape: buttonShape,
-                      child: Text("reply"),
-                    ),
-                  ],
-                ),
-              ],
-            )
-        )
-    );
+                ))));
   }
 }
